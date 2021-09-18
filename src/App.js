4af7a180ce953b4553c9bb536798d02a23628f56
Bhargavi <bhargavishetty113@gmail.com>
2021-09-18 12:40:21 +0530

    added folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import Home from "./Component/Home/home";
 
 function App() {
-  return <></>;
+  return <>
+  <Home/>
+    </>;
 }
 
 export default App;
---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            welcome Home
+        </div>
+    )
+}
+
+export default Home
